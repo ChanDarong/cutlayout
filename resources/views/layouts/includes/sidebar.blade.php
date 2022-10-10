7eb5bc64c28a0d7769487650d92bd34b45682f7e
ChanDarong <chandarong333@gmail.com>
2022-10-10 09:23:25 +0700

    Change Sidebar

--- resources/views/layouts/includes/sidebar.blade.php
+++ resources/views/layouts/includes/sidebar.blade.php
@@ -10,15 +10,16 @@
 
                 <li>
                     <a href="javascript: void(0);" class="waves-effect">
-                        <i class="bx bx-home-circle"></i><span class="badge badge-pill badge-info float-right">04</span>
+                        <i class="bx bx-home-circle"></i>
+                        {{-- <span class="badge badge-pill badge-info float-right">04</span> --}}
                         <span key="t-dashboards">Dashboards</span>
                     </a>
-                    <ul class="sub-menu" aria-expanded="false">
+                    {{-- <ul class="sub-menu" aria-expanded="false">
                         <li><a href="index.html" key="t-default">Default</a></li>
                         <li><a href="dashboard-saas.html" key="t-saas">Saas</a></li>
                         <li><a href="dashboard-crypto.html" key="t-crypto">Crypto</a></li>
                         <li><a href="dashboard-blog.html" key="t-blog">Blog</a></li>
-                    </ul>
+                    </ul> --}}
                 </li>
 
                 <li>
